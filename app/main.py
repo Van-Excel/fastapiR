1f7553a5ddd209ba146d8c4629b6c064cd4fca66
Van-Excel <vanexcel.investments@gmail.com>
2024-11-27 02:26:11 +0000

    API for retrieving specific resources

--- app/main.py
+++ app/main.py
@@ -19,11 +19,10 @@ async def lifespan(app: FastAPI):
     app.state.db_connection = db_connection
     print("Database connection established")
     try:
-        yield 
+        yield
     finally:
         db_connection.close()  # Close the connection
         print("Database connection closed")
-
 
 
 app = FastAPI(
@@ -33,8 +32,9 @@ app = FastAPI(
     version="0.1.0",
 )
 
+
 # returns DB connection
-def get_db(request:Request):
+def get_db(request: Request):
     return request.app.state.db_connection
 
 
@@ -43,68 +43,102 @@ async def root():
 
     return {"message": "Hello World Programmers"}
 
-
-
-@app.get('/products', response_model=List[ProductResponse], status_code=status.HTTP_200_OK, tags=["Products"]) 
-async def get_posts(db= Depends(get_db)):
+# retrieve a list of products
+@app.get(
+    "/products",
+    response_model=List[ProductResponse],
+    status_code=status.HTTP_200_OK,
+    tags=["Products"],
+)
+async def get_products(db=Depends(get_db)):
     with db.cursor() as cur:
         cur.execute("SELECT * FROM products")
         products = cur.fetchall()
     return [ProductResponse(**product) for product in products]
 
-
-@app.get('/posts', response_model=List[PostsResponse], status_code=status.HTTP_200_OK, tags=["Posts"]) 
-async def get_posts(db= Depends(get_db)):
+# retrieve a list of posts
+@app.get(
+    "/posts",
+    response_model=List[PostsResponse],
+    status_code=status.HTTP_200_OK,
+    tags=["Posts"],
+)
+async def get_posts(db=Depends(get_db)):
     with db.cursor() as cur:
         cur.execute('SELECT * FROM public."Posts"')
         posts = cur.fetchall()
     return [PostsResponse(**post) for post in posts]
 
 
-
-
-# create new products
-
-@app.post("/products", status_code=status.HTTP_201_CREATED, response_model= ProductResponse, tags=["Products"])
-async def create_post(payload: CreateProduct, db= Depends(get_db)):
+# create a new product
+@app.post(
+    "/products",
+    status_code=status.HTTP_201_CREATED,
+    response_model=ProductResponse,
+    tags=["Products"],
+)
+async def create_product(payload: CreateProduct, db=Depends(get_db)):
     payload = payload.model_dump()
     try:
-        
+
         with db.cursor() as cur:
-            query = '''INSERT INTO products (name, price, is_sale, inventory) VALUES (%s, %s, %s, %s) RETURNING *; '''
-            values = (payload["name"], payload["price"] , payload["is_sale"], payload["inventory"])
+            query = """INSERT INTO products (name, price, is_sale, inventory) VALUES (%s, %s, %s, %s) RETURNING *; """
+            values = (
+                payload["name"],
+                payload["price"],
+                payload["is_sale"],
+                payload["inventory"],
+            )
             cur.execute(query, values)
-            
+
             # Commit the transaction to save changes to the database
             db.commit()
-            
+
             # fetch the created post since the transaction is committed
             new_post = dict(cur.fetchone())
-            
+
         return ProductResponse(**new_post)
     except Exception as e:
         # Rollback the transaction if any error occurs
         db.rollback()
         print(f"Error occurred: {e}")
         # You can raise an HTTP exception or return an error message
-        raise HTTPException(status_code=500, detail="Failed to create product")
-            
-        
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create product")
+
+# retrieve a specific product
+@app.get("/products/{id}", status_code=status.HTTP_200_OK, response_model=ProductResponse, tags=["Products"])
+async def get_specific_product(id: str, db= Depends(get_db)):
+    try:
+        with db.cursor() as cur:
+            query = 'SELECT name, price, is_sale, inventory, created_at FROM products WHERE id=%s'
+            identifier = id
+            cur.execute(query, identifier)
+            product = dict(cur.fetchone())
     
-   
+        return ProductResponse(**product)
+            
+    except Exception as e:
+        print("Error occurred:", e)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found")
 
 
+# retrieve a specific post
+@app.get("/posts/{id}", status_code=status.HTTP_200_OK, response_model=PostsResponse, tags=["Posts"])
+async def get_specific_product(id: str, db= Depends(get_db)):
+    try:
+        with db.cursor() as cur:
+            query = 'SELECT title, content, published,  created_at FROM public."Posts" WHERE id=%s'
+            identifier = id
+            cur.execute(query, identifier)
+            post = dict(cur.fetchone())
+    
+        return PostsResponse(**post)
+            
+    except Exception as e:
+        print("Error occurred:", e)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found")
 
 
-@app.get("/posts/{id}", status_code=status.HTTP_200_OK, tags=["Post"])
-async def get_specific_user(id: str):
-    filtered_data = [post for post in data if post["id"] == id]
-    if not filtered_data:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail={"message": f"Post with id of {id} not found"},
-        )
-    return {"data": filtered_data}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Post"])
